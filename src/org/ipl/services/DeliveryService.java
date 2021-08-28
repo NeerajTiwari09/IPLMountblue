ecb069e09c9287ef80cf7f1a31c9cde93f25e090
NeerajTiwari17 <neeraj.tiwari.17.1@mountblue.tech>
2021-08-28 18:34:15 +0530

    Top Economical Bowlers Added

--- src/org/ipl/services/DeliveryService.java
+++ src/org/ipl/services/DeliveryService.java
@@ -26,6 +26,7 @@ public class DeliveryService {
         List<Map<String, String>> listDelivery = dao.getDataFromDeliveriesCsv();
         List<Integer> idList = getMatchId(listMatch, year);
         Map<String, Integer> teamGotExtraRunMap = new HashMap<>();
+//        Integer[] idList = new Integer[]{629,619,612,626,579,583,586,589,594,622,597,604,608,615};
         for (Integer i : idList) {
             for (Map<String, String> map : listDelivery) {
                 if (Integer.valueOf(map.get("match_id")).equals(i)) {
@@ -42,31 +43,40 @@ public class DeliveryService {
         return teamGotExtraRunMap;
     }
 
-    /**********************
-     *Has some bugs
-     *Not completed yet
-     *Find the most Economical bowlers
-     * *********************/
-    public Map<String, Integer> getTheTopEconomicalBowlers(int year) {
+    public Map<String, Float> getTheTopEconomicalBowlers(int year) {
         List<Map<String, String>> listMatch = dao.getDataFromMatchesCsv();
         List<Map<String, String>> listDelivery = dao.getDataFromDeliveriesCsv();
         List<Integer> idList = getMatchId(listMatch, year);
-        System.out.println(idList);
-        Map<String, Integer> topEconomicalBowlerMap = new HashMap<>();
+        Map<String, Integer> totalBalls = new HashMap<>();
+        List<String> name = new ArrayList<>();
+        Map<String, Integer> bowlerRunsMap = new HashMap<>();
         for (Integer i : idList) {
-            for (Map<String, String> map : listDelivery) {
+            for (Map<String, String> map : listDelivery) { //  TODO - needs optimization here
                 if (Integer.valueOf(map.get("match_id")).equals(i)) {
                     String bowlerName = map.get("bowler");
-                    if (!topEconomicalBowlerMap.containsKey(bowlerName)) {
-                        topEconomicalBowlerMap.put(bowlerName, Integer.valueOf(map.get("total_runs")));
+                    if (!bowlerRunsMap.containsKey(bowlerName)) {
+                        bowlerRunsMap.put(bowlerName, Integer.valueOf(map.get("total_runs")));
+                        totalBalls.put(bowlerName, 1);
+                        name.add(bowlerName);
                     } else {
-                        topEconomicalBowlerMap.put(bowlerName, topEconomicalBowlerMap.get(bowlerName) + Integer.valueOf(map.get("total_runs")));
+                        bowlerRunsMap.put(bowlerName, bowlerRunsMap.get(bowlerName) + Integer.valueOf(map.get("total_runs")));
+                        totalBalls.put(bowlerName, totalBalls.get(bowlerName)+1);
                     }
                 }
             }
-//            break;
         }
-        System.out.println(topEconomicalBowlerMap);
-        return topEconomicalBowlerMap;
+        return getTheTopEconomicalBowlers (bowlerRunsMap, totalBalls, name);
     }
+
+    private Map<String, Float> getTheTopEconomicalBowlers(Map<String, Integer> bowlerRunsMap, Map<String, Integer> totalBalls, List<String> listOfBowers){
+        Map<String, Float> topEconomicBowlers = new HashMap<>();
+        for(String bName : listOfBowers){
+            float rate = ((float)bowlerRunsMap.get(bName) / (float)totalBalls.get(bName))*6;
+            topEconomicBowlers.put(bName, rate);
+//            System.out.println(topEconomicBowlers);
+
+        }
+        return topEconomicBowlers;
+    }
+
 }
